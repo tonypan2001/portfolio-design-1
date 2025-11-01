be5a07118b1c7be2f52b72c4c5b06a531fbad6d7
Prompan Uechanwech <panzaza9543@gmail.com>
2025-11-01 11:29:47 +0700

    edit logo

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -1,37 +1,21 @@
 import * as React from "react";
 import { cn } from "@/lib/utils";
 
-type LogoProps = React.SVGProps<SVGSVGElement> & {
+type LogoProps = React.ImgHTMLAttributes<HTMLImageElement> & {
   className?: string;
   title?: string;
 };
 
 export function Logo({ className, title = "PanStudio", ...props }: LogoProps) {
   return (
-    <svg
-      viewBox="0 0 120 40"
-      xmlns="http://www.w3.org/2000/svg"
-      role="img"
+    <img
+      src="/icon.svg"
+      alt={`${title} logo`}
       aria-label={`${title} logo`}
       className={cn("block", className)}
-      fill="currentColor"
       {...props}
-    >
-      <title>{title}</title>
-      {/* Stylized "PS" using text for simplicity; inherits currentColor */}
-      <text
-        x="0"
-        y="29"
-        fontFamily="Geist, Segoe UI, Arial, Helvetica, sans-serif"
-        fontWeight="800"
-        fontSize="30"
-        letterSpacing="-1"
-      >
-        PS
-      </text>
-    </svg>
+    />
   );
 }
 
 export default Logo;
-
